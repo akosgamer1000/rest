923795ca808b56f78ed4d213661d69a4cebdbf8a
Petrik Diak <diak@petrik.hu>
2024-10-18 15:39:29 +0200

    bxc

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller,  Delete,  Get, NotFoundException, Param, Patch, Put, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
+import { Replace } from './k.dto';
+
 
 @Controller()
 export class AppController {
@@ -12,4 +15,54 @@ export class AppController {
       message: this.appService.getHello()
     };
   }
+  #productlist=[
+    {
+
+      name:  'washing machine',
+      price:15000
+
+    },
+    {
+
+      name:  'baseball',
+      price:10000
+
+    },
+    {
+
+      name:  'detergent',
+      price:10000
+
+    }
+    
+    
+  ]
+  @Get('product')
+  listprod(){
+    return  this.#productlist;
+      
+  
+  }
+  @Get('product/:id')
+  getPar(@Param('id') id:string){
+      return JSON.stringify(this.#productlist[id])
+  }
+  @Delete('product/:id')
+  Dleteprod(@Param('id') id:string){
+      if(!this.#productlist[id]){
+        throw new NotFoundException("cannot delte")
+      }
+     
+      this.#productlist.splice(Number.parseInt( id),1)
+  }
+  @Put('product/:id')
+  getPut(@Param('id') id:string,@Body() data:Replace ){
+    if(!this.#productlist[id]){
+      throw new NotFoundException("cannot delte")
+    }
+    this.#productlist[id].name =data.name
+    this.#productlist[id].price=data.price
+    
+  }
 }
+
---
+++ src/k.dto.ts
@@ -0,0 +1,5 @@
+export interface Replace{
+	name:string
+  price:number
+}
+
